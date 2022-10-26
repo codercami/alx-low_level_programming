dea74a99bca6ae5f886cf145c369a0bd826db7dc
codercami <camiexodus1515@gmail.com>
2022-10-26 02:59:35 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,8 +11,8 @@ void puts2(char *str)
 {
 	int string;
 
-	for (string = 0; str[string] != '\0';
-			if (string % 2 == 0)
+	for (string = 0; str[string] != '\0'; string++)
+		if (string % 2 == 0)
 			_putchar(str[string]);
 			_putchar('\n');
 			}
